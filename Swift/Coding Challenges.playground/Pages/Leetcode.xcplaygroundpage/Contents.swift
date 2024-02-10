2e98a35a872329a8fa071e56c2bc29ddcaf0ea77
Samuel Folledo <samuelfolledo@gmail.com>
2024-02-09 17:58:23 -0700

    Added next challenge

--- Swift/Coding Challenges.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
@@ -80,4 +80,18 @@ func testAddTwoNumbers() {
 testAddTwoNumbers()
 
 
+/*:
+ ## 21. [Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists/description/)
+ You are given the heads of two sorted linked lists list1 and list2.
+
+ Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+ Return the head of the merged linked list.
+
+ Constraints:
+ - The number of nodes in both lists is in the range [0, 50].
+ - -100 <= Node.val <= 100
+ - Both list1 and list2 are sorted in non-decreasing order.
+ */
+
 //: [Next](@next)
