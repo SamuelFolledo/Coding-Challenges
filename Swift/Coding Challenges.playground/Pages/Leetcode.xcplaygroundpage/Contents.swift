9ea21416b0de4daea2a9a858d6d1156953f72ce7
Samuel Folledo <samuelfolledo@gmail.com>
2024-02-14 22:07:57 -0700

    add prompt for challenge 32

--- Swift/Coding Challenges.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
@@ -166,4 +166,34 @@ func testMergeTwoLists() {
 
 testMergeTwoLists()
 
+/*:
+ ## 32. [Longest Valid Parentheses](https://leetcode.com/problems/longest-valid-parentheses/description/)
+ Hard
+ Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses
+ substring.
+
+ #### Example 1:
+ Input: s = "(()"
+ Output: 2
+ Explanation: The longest valid parentheses substring is "()".
+
+ #### Example 2:
+ Input: s = ")()())"
+ Output: 4
+ Explanation: The longest valid parentheses substring is "()()".
+
+ #### Example 3:
+ Input: s = ""
+ Output: 0
+
+ #### Constraints:
+ 0 <= s.length <= 3 * 104
+ s[i] is '(', or ')'.
+ */
+
+func longestValidParentheses(_ s: String) -> Int {
+    
+    return 0
+}
+
 //: [Next](@next)
