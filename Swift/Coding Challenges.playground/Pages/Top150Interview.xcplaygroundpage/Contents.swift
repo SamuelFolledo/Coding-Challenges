61c0b48637febc3d60096277f7fde843c13f86bc
Samuel Folledo <samuelfolledo@gmail.com>
2025-01-07 04:57:07 -0500

    [add] remove element solution

--- Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
@@ -3,6 +3,7 @@
 import Foundation
 
 //1) 88. Merge Sorted Array
+//You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 //Input: nums1 = [1,2,3], m = 3, nums2 = [2,5,6], n = 3
 //Output: [1,2,2,3,5,6]
 func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
@@ -28,5 +29,20 @@ func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
             }
         }
     }
+}
+
+/* 2) 27. Remove Element
+ Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+*/
+func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+    var index = 0
+    var nums2 = nums
+    for i in 0..<nums.count {
+        if nums2[i] != val {
+            nums[index] = nums[i]
+            index += 1
+        }
+    }
+    return index
 }
 //: [Next](@next)
