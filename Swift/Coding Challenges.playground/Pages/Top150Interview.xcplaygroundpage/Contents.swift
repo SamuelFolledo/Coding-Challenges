a0ebfa57c8a9b4d731ee31eb769a2603fbfa534f
Samuel Folledo <samuelfolledo@gmail.com>
2025-01-10 21:23:14 -0500

    add brute force solution for Rotate Array

--- Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
@@ -169,4 +169,19 @@ func majorityElement(_ nums: [Int]) -> Int {
     }
     return candidate
 }
+
+/*
+ 6) 189. Rotate Array
+ Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+ Input: nums = [1,2,3,4,5,6,7], k = 3
+ Output: [5,6,7,1,2,3,4]
+ */
+func rotateBrute(_ nums: inout [Int], _ k: Int) {
+    guard nums.count > 1 else { return }
+    for _ in 0..<k {
+        nums.insert(nums.popLast()!, at: 0)
+    }
+}
+
+
 //: [Next](@next)
