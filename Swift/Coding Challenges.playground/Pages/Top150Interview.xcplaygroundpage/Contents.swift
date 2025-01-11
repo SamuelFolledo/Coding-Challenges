936e40f65763112340c39a843fa8cff4dbecad67
Samuel Folledo <samuelfolledo@gmail.com>
2025-01-10 21:35:27 -0500

    add better rotation solution

--- Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
@@ -183,5 +183,23 @@ func rotateBrute(_ nums: inout [Int], _ k: Int) {
     }
 }
 
+func rotate(_ nums: inout [Int], _ k: Int) {
+    guard !nums.isEmpty ||  k > 0  else { return }
+
+    //pointers
+    let n = nums.count
+    let k = k % n
+
+    // Reverse entire array
+    nums.reverse()
+
+    // Reverse first k elements
+    nums[0..<k].reverse()
+
+    // Reverse remaining n-k elements
+    nums[k..<n].reverse()
+}
+var rotateArr = [1,2,3,4,5,6,7]
+rotate(&rotateArr, 3)
 
 //: [Next](@next)
