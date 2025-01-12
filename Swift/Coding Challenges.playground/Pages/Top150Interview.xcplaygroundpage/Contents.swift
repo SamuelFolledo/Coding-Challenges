f49d0accfc95f290edcdd3a3b8fadf4a86551f7e
Samuel Folledo <samuelfolledo@gmail.com>
2025-01-12 16:18:40 -0500

    added solution to buy and sell stock 2

--- Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
@@ -247,4 +247,35 @@ func maxProfit(_ prices: [Int]) -> Int {
     }
     return maxProfit
 }
+
+/*
+ 122. Best Time to Buy and Sell Stock II
+ You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+ On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+ Find and return the maximum profit you can achieve.
+
+ Example 1:
+ Input: prices = [7,1,5,3,6,4]
+ Output: 7
+
+ Example 2:
+ Input: prices = [1,2,3,4,5]
+ Output: 4
+ */
+
+//O(n)
+func maxProfit2(_ prices: [Int]) -> Int {
+    guard !prices.isEmpty else { return 0 }
+
+    var maxProfit = 0
+    for i in 1..<prices.count {
+        let price = prices[i]
+        let previousPrice = prices[i - 1]
+        ///only increment maxProfit if current price is greater than previous price
+        guard price > previousPrice else { continue }
+        maxProfit += price - previousPrice
+    }
+    return maxProfit
+}
+
 //: [Next](@next)
