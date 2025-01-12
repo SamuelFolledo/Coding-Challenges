b11bfe0b6362580d03a6064f1b42f3796c1ff4ab
Samuel Folledo <samuelfolledo@gmail.com>
2025-01-12 18:25:21 -0500

    add best solution for isSubsequence

--- Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
@@ -376,4 +376,16 @@ func isSubsequenceBrute(_ s: String, _ t: String) -> Bool {
     }
     return false
 }
+
+func isSubsequence(_ s: String, _ t: String) -> Bool {
+    var i = s.startIndex
+    var j = t.startIndex
+    while i < s.endIndex && j < t.endIndex {
+        if s[i] == t[j] {
+            i = s.index(after: i)
+        }
+        j = t.index(after: j)
+    }
+    return i == s.endIndex
+}
 //: [Next](@next)
