65bf04e3a9b712bd28716e27f0464117d6f78d66
Samuel Folledo <samuelfolledo@gmail.com>
2025-01-12 19:13:58 -0500

    add solution to 3Sum

--- Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
@@ -436,4 +436,80 @@ func twoSum2(_ numbers: [Int], _ target: Int) -> [Int] {
     return []
 }
 
+/*
+ 29) 3Sum
+ Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ Notice that the solution set must not contain duplicate triplets.
+
+ Example 1:
+ Input: nums = [-1,0,1,2,-1,-4]
+ Output: [[-1,-1,2],[-1,0,1]]
+ Explanation:
+ nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+ nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+ nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+ The distinct triplets are [-1,0,1] and [-1,-1,2].
+ Notice that the order of the output and the order of the triplets does not matter.
+
+ Example 2:
+ Input: nums = [0,1,1]
+ Output: []
+ Explanation: The only possible triplet does not sum up to 0.
+
+ Example 3:
+ Input: nums = [0,0,0]
+ Output: [[0,0,0]]
+ Explanation: The only possible triplet sums up to 0.
+ */
+func threeSumBrute(_ nums: [Int]) -> [[Int]] {
+    let sortedNums = nums.sorted()
+    var result = Set<[Int]>()
+    for i in 0 ..< sortedNums.count {
+        var j = i + 1
+        var k = sortedNums.count - 1
+        while j < k {
+            let sum = sortedNums[i] + sortedNums[j] + sortedNums[k]
+            if sum == 0 {
+                result.insert([sortedNums[i], sortedNums[j], sortedNums[k]])
+                j += 1
+                k -= 1
+            } else if sum < 0 {
+                j += 1
+            } else {
+                k -= 1
+            }
+        }
+    }
+    return Array(result)
+}
+
+func threeSum(_ nums: [Int]) -> [[Int]] {
+    let sortedNums = nums.sorted()
+    if sortedNums.count < 3 { return [] }
+    if sortedNums.last! < 0 { return [] }
+    var result = [[Int]]()
+    for i in 0..<sortedNums.count {
+        if sortedNums[i] > 0 { break }
+        if i > 0 && sortedNums[i] == sortedNums[i-1] { continue }
+        var low = i + 1
+        var high = sortedNums.count - 1
+        var sum = 0
+        while low < high {
+            sum = sortedNums[i] + sortedNums[low] + sortedNums[high]
+            if sum < 0 {
+                low += 1
+            } else if sum > 0 {
+                high -= 1
+            } else {
+                result.append([sortedNums[i], sortedNums[low], sortedNums[high]])
+                low += 1
+                high -= 1
+                while low < high && sortedNums[low] == sortedNums[low - 1] { low += 1 }
+            }
+        }
+    }
+    return result
+}
+
+
 //: [Next](@next)
