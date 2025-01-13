224ae59a8823920d54b43b23e990c03fa954eaf6
Samuel Folledo <samuelfolledo@gmail.com>
2025-01-13 12:33:47 -0500

    add solution to invert binary tree

--- Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
@@ -857,6 +857,36 @@ func maxDepth(_ root: TreeNode?) -> Int {
     return 1 + max(maxDepth(root.left), maxDepth(root.right))
 }
 
+/*
+ 226. Invert Binary Tree = Easy
+ Given the root of a binary tree, invert the tree, and return its root.
+
+ Example 1:
+ Input: root = [4,2,7,1,3,6,9]
+ Output: [4,7,2,9,6,3,1]
+
+ Example 2:
+ Input: root = [2,1,3]
+ Output: [2,3,1]
+ Explanation:
+     [2]            [2]
+    /   \   ->     /   \
+ [1]    [3]     [3]     [1]
+
+ Example 3:
+ Input: root = []
+ Output: []
+ */
+
+func invertTree(_ root: TreeNode?) -> TreeNode? {
+    guard let root = root else {return nil}
+    let left = invertTree(root.left)
+    let right = invertTree(root.right)
+    root.left = right
+    root.right = left
+    return root
+}
+
 //MARK: - Binary Tree BFS
 
 //MARK: - Binary Search Tree
