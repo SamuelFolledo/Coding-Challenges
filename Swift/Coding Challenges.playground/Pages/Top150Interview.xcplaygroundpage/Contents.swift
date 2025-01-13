e19987e984120b9434cbf8ea035bccc454fdd709
Samuel Folledo <samuelfolledo@gmail.com>
2025-01-13 11:52:20 -0500

    add solution to spiral matrix 1

--- Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
@@ -653,4 +653,64 @@ func isValidSudoku(_ board: [[Character]]) -> Bool {
     }
     return true
 }
+
+/*
+ 54. Spiral Matrix
+ Given an m x n matrix, return all elements of the matrix in spiral order.
+
+ Example 1:
+ Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ Output: [1,2,3,6,9,8,7,4,5]
+
+ Example 2:
+ Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ */
+
+func spiralOrder(_ matrix: [[Int]]) -> [Int] {
+    guard !(matrix.isEmpty) else { return [] }
+
+    var result: [Int] = []
+    var rBegin = 0
+    var rEnd = matrix.count - 1
+    var cBegin = 0
+    var cEnd = matrix[0].count - 1
+
+    while rBegin <= rEnd && cBegin <= cEnd {
+        // Traverse right
+        for i in stride(from: cBegin, to: cEnd + 1, by: 1) {
+            print("right to \(matrix[rBegin][i])")
+            result.append(matrix[rBegin][i])
+        }
+        rBegin += 1 //move row begin down
+
+        // Traverse down
+        for i in stride(from: rBegin, to: rEnd + 1, by: 1) {
+            print("down to \(matrix[i][cEnd])")
+            result.append(matrix[i][cEnd])
+        }
+        cEnd -= 1 //move column end to the left
+
+        // Traverse left
+        if rBegin <= rEnd {
+            for i in stride(from: cEnd, to: cBegin - 1, by: -1) {
+                print("LEFT to \(matrix[rEnd][i])")
+                result.append(matrix[rEnd][i])
+            }
+        }
+        rEnd -= 1 //move row end up
+
+        // Traverse up
+        if cBegin <= cEnd {
+            for i in stride(from: rEnd, to: rBegin - 1, by: -1) {
+                print("up to \(matrix[i][cBegin])")
+                result.append(matrix[i][cBegin])
+            }
+        }
+        cBegin += 1 //more column begin to the right
+    }
+    return result
+}
+
+//spiralOrder([[1,2,3],[4,5,6],[7,8,9]])
 //: [Next](@next)
