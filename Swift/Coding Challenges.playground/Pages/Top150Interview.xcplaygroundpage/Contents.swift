7d1450e7db2c0df725724a7a7f83fe4f1d6a46ec
Samuel Folledo <samuelfolledo@gmail.com>
2025-01-20 13:51:01 -0500

    add a binary search insert position

--- Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/Top150Interview.xcplaygroundpage/Contents.swift
@@ -1044,6 +1044,51 @@ func levelOrder(_ root: TreeNode?) -> [[Int]] {
 //MARK: - Binary Search Tree
 
 /*
+ 35. Search Insert Position = Easy
+ Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+ You must write an algorithm with O(log n) runtime complexity.
+
+ Example 1:
+ Input: nums = [1,3,5,6], target = 5
+ Output: 2
+
+ Example 2:
+ Input: nums = [1,3,5,6], target = 2
+ Output: 1
+
+ Example 3:
+ Input: nums = [1,3,5,6], target = 7
+ Output: 4
+ */
+
+func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+    var left = 0
+    var right = nums.count - 1
+    while left <= right {
+        let mid = (left + right) / 2
+        if target == nums[mid] {
+            return mid
+        } else if target < nums[mid] {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+    return left
+}
+
+
+func searchInsert2(_ nums: [Int], _ target: Int) -> Int {
+    var lhs = 0, rhs = nums.count
+    while lhs < rhs {
+        let mid = (lhs + rhs) / 2
+        nums[mid] < target ? (lhs = mid + 1) : (rhs = mid)
+    }
+    return lhs
+}
+
+/*
  4. Median of Two Sorted Arrays = Hard
  Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
  The overall run time complexity should be O(log (m+n)).
