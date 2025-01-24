d117a13c86960ae920691983316908c02e47c381
Samuel Folledo <samuelfolledo@gmail.com>
2025-01-24 16:43:25 -0500

    add solution to  maximum gap

--- Swift/Coding Challenges.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
@@ -280,4 +280,32 @@ func testCombinationSum() {
     print(combinationSum([2], 1)) //Output: []
 }
 testCombinationSum()
+
+/*
+ 164. Maximum Gap = Medium
+ Given an integer array nums, return the maximum difference between two successive elements in its sorted form. If the array contains less than two elements, return 0.
+
+ You must write an algorithm that runs in linear time and uses linear extra space.
+
+ Example 1:
+ Input: nums = [3,6,9,1]
+ Output: 3
+ Explanation: The sorted form of the array is [1,3,6,9], either (3,6) or (6,9) has the maximum difference 3.
+
+ Example 2:
+ Input: nums = [10]
+ Output: 0
+ Explanation: The array contains less than 2 elements, therefore return 0.
+ */
+func maximumGap(_ nums: [Int]) -> Int {
+    guard nums.count > 1 else { return 0 }
+    let nums = nums.sorted()
+    var maxDifference = 0
+    for i in 1..<nums.count {
+        if maxDifference < nums[i] - nums[i - 1] {
+            maxDifference = nums[i] - nums[i - 1]
+        }
+    }
+    return maxDifference
+}
 //: [Next](@next)
