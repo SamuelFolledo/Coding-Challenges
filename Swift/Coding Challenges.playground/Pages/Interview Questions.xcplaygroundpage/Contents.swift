28ba6a2b38226265471c782f318b932d9b671ec4
Samuel Folledo <samuelfolledo@gmail.com>
2025-01-25 15:03:32 -0500

    add solution to matrix

--- Swift/Coding Challenges.playground/Pages/Interview Questions.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/Interview Questions.xcplaygroundpage/Contents.swift
@@ -110,6 +110,43 @@ func setZeroes(_ matrix: inout [[Int]]) {
     }
 }
 
+/*
+ MARK: - 74. Search a 2D Matrix = Medium
+ You are given an m x n integer matrix matrix with the following two properties:
+ Each row is sorted in non-decreasing order.
+ The first integer of each row is greater than the last integer of the previous row.
+ Given an integer target, return true if target is in matrix or false otherwise.
+
+ You must write a solution in O(log(m * n)) time complexity.
+
+ Example 1:
+ Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+ Output: true
+
+ Example 2:
+ Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+ Output: false
+ */
+
+func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+    if matrix.isEmpty { return false }
+    var row = 0
+    //get the row
+    for x in 0..<matrix.count {
+        if target >= matrix[x].first! && target <= matrix[x].last! {
+            row = x
+            break
+        }
+    }
+    //get the column
+    for y in 0..<matrix[row].count {
+        if matrix[row][y] == target {
+            return true
+        }
+    }
+    return false
+}
+
 //MARK: - Vehicle
 
 class Vehicle {
