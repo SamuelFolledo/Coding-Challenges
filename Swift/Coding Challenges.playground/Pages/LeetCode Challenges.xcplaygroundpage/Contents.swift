437ce1ae5ec48ff444ab94ed6e09a28fb94b596e
Samuel Folledo <samuelfolledo@gmail.com>
2025-01-28 01:30:28 -0500

    add solution to single number

--- Swift/Coding Challenges.playground/Pages/LeetCode Challenges.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/LeetCode Challenges.xcplaygroundpage/Contents.swift
@@ -250,4 +250,35 @@ let memoisedStart = Date()
 let memoisedFib = fibMemoised(fibNum)
 let memoisedEnd = Date()
 print("Memoised: \(memoisedFib) in \(memoisedStart.distance(to: memoisedEnd)) seconds")
+
+/*
+ MARK: 136. Single Number = Easy
+ Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+ You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ Example 1:
+ Input: nums = [2,2,1]
+ Output: 1
+
+ Example 2:
+ Input: nums = [4,1,2,1,2]
+ Output: 4
+
+ Example 3:
+ Input: nums = [1]
+ Output: 1
+ */
+
+func singleNumber(_ nums: [Int]) -> Int {
+    var numbers = Set<Int>()
+    for num in nums {
+        if numbers.contains(num) {
+            numbers.remove(num)
+        } else {
+            numbers.insert(num)
+        }
+    }
+    return numbers.first!
+}
 //: [Next](@next)
