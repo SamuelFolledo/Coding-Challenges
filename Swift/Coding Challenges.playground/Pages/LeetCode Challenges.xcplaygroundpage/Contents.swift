2c17c9d168c7c6999da98f9ec1e2e10363f5323f
Samuel Folledo <samuelfolledo@gmail.com>
2025-01-28 01:44:24 -0500

    add solution to missing ranges

--- Swift/Coding Challenges.playground/Pages/LeetCode Challenges.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/LeetCode Challenges.xcplaygroundpage/Contents.swift
@@ -281,4 +281,40 @@ func singleNumber(_ nums: [Int]) -> Int {
     }
     return numbers.first!
 }
+
+/*
+ MARK: 163. Missing Ranges = Easy
+ You are given an inclusive range [lower, upper] and a sorted unique integer array nums, where all elements are within the inclusive range.
+ A number x is considered missing if x is in the range [lower, upper] and x is not in nums.
+ Return the shortest sorted list of ranges that exactly covers all the missing numbers. That is, no element of nums is included in any of the ranges, and each missing number is covered by one of the ranges.
+
+ Example 1:
+ Input: nums = [0,1,3,50,75], lower = 0, upper = 99
+ Output: [[2,2],[4,49],[51,74],[76,99]]
+ Explanation: The ranges are:
+ [2,2]
+ [4,49]
+ [51,74]
+ [76,99]
+
+ Example 2:
+ Input: nums = [-1], lower = -1, upper = -1
+ Output: []
+ Explanation: There are no missing ranges since there are no missing numbers.
+ */
+
+func findMissingRanges(_ nums: [Int], _ lower: Int, _ upper: Int) -> [[Int]] {
+    guard nums.isEmpty == false else { return [[lower, upper]] }
+    var result = [[Int]]()
+    //fill in the gaps between nums
+    for i in 0..<nums.count-1 {
+        if(nums[i]+1 != nums[i+1]) {
+            result.append([nums[i]+1, nums[i+1]-1])
+        }
+    }
+    //populate the beginning and end from lower to first number and last number to upper
+    if nums.first != lower { result.insert(([lower, (nums.first ?? 0) - 1]), at: 0)}
+    if nums.last != upper { result.append([(nums.last ?? 0) + 1, upper])}
+    return result
+}
 //: [Next](@next)
