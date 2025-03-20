f9707a6cd1e9bff5b9d3ed6012fd293869c44b7e
Samuel Folledo <samuelfolledo@gmail.com>
2025-03-20 19:30:43 -0400

    add solution to reverse vowels

--- Swift/Coding Challenges.playground/Pages/LeetCode Challenges.xcplaygroundpage/Contents.swift
+++ Swift/Coding Challenges.playground/Pages/LeetCode Challenges.xcplaygroundpage/Contents.swift
@@ -387,4 +387,43 @@ func longestCommonPrefix(_ strs: [String]) -> String {
     }
     return prefix
 }
+
+/*
+ 345. Reverse Vowels of a String = Easy
+ Given a string s, reverse only all the vowels in the string and return it.
+
+ The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+ Example 1:
+ Input: s = "IceCreAm"
+ Output: "AceCreIm"
+ Explanation: The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+ Example 2:
+ Input: s = "leetcode"
+ Output: "leotcede"
+ */
+func reverseVowels(_ s: String) -> String {
+    //have 2 pointers from beginning to end, and whenever vowel is seen, swap the vowels
+    guard s.count > 1 else { return s }
+    var start = 0
+    var end = s.count - 1
+    let vowels: Set<Character> = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    var s = Array(s)
+    while start < end {
+        if vowels.contains(s[start]) && vowels.contains(s[end]) {
+            s.swapAt(start, end)
+            start += 1
+            end -= 1
+        } else if vowels.contains(s[start]) {
+            end -= 1
+        } else if vowels.contains(s[end]) {
+            start += 1
+        } else {
+            start += 1
+            end -= 1
+        }
+    }
+    return String(s)
+}
 //: [Next](@next)
